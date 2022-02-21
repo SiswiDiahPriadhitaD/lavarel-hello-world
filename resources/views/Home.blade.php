bd2abe15cdbd5e579def218fb9c2381f95e390ed
= <=>
2022-02-21 06:13:55 -0800

    Praktikum3

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Perusahaan Listrik Negara</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse ">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="Pictures/LOGO.png">PLN</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="/">Home</a></li>
+      <li class="active"><a href="/products">Products</a></li>
+      <li class="active"><a href="/news">News</a></li>
+      <li class="active"><a href="/program">Program</a></li>
+      <li class="active"><a href="/about">About Us</a></li>
+
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
+  
+<div class="container">
+	<div class="row">
+    <div >
+      <h3 >INFORMASI</h3>
+      Bagian ini memberi semua informasi terkini untuk membantu Anda melakukan transaksi listrik PLN dengan lebih efisien. Anda dapat memilih untuk pasang listrik baru, melakukan perubahan daya, dan melakukan sambungan listrik sementara maupun pengaduan gangguan dan layanan listrik lainnya. Melakukan transaksi listrik PLN tidak pernah semudah ini dengan segala sesuatu di bawah satu atap.
+      </p>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
